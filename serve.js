3086028a8f57d463c2cd0c29bebcab4f305ca23f
Simon Wheelwright <mister_simon@hotmail.co.uk>
2016-03-03 20:46:13 +0000

    Commiting the server + Procfile for Herokuuuuuuu.

---
+++ serve.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+
+var serveIndex = require('serve-index'),
+	express = require('express'),
+	path = require('path'),
+	app = express();
+
+// app.use('/', serveIndex(path.join(__dirname, 'scripts')));
+app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
+
+app.get('/listing/lua', function(req, res){
+	var files = [],
+		stringyFiles = "";
+
+	checkFolder('scripts', files);
+
+	stringyFiles = JSON.stringify(files).replace('[', '{').replace(']', '}');
+
+	res.send(stringyFiles);
+});
+
+app.listen(8888, function () { console.log('WE GO NOW'); });
+
+
+function checkFolder(path, output){
+	if(fs.statSync(path).isDirectory()){
+		var contents = fs.readdirSync(path);
+
+		for (var i = 0, l = contents.length; i < l; i++) {
+			checkFolder(path+'/'+contents[i], output);
+		}
+	} else {
+		output.push(path);
+	}
+}
