74727d1ac27d3b2fc440aea74b0e85e66b2427dd
Simon Wheelwright <mister_simon@hotmail.co.uk>
2016-03-05 02:26:09 +0000

    Nope. We can't keep things in memory on heroku persistently.

--- serve.js
+++ serve.js
@@ -4,10 +4,7 @@ var serveIndex = require('serve-index'),
 	express = require('express'),
 	path = require('path'),
 	app = express();
-
-var storedshit = "NOPE";
-
-// app.use('/', serveIndex(path.join(__dirname, 'scripts')));
+	
 app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
 
 app.get('/listing/lua', function(req, res){
@@ -19,15 +16,6 @@ app.get('/listing/lua', function(req, res){
 	stringyFiles = JSON.stringify(files).replace('[', '{').replace(']', '}');
 
 	res.send(stringyFiles);
-});
-
-app.get('/storeshit', function(req, res){
-	storedshit = "YEP";
-	res.send("STORED YEP");
-});
-
-app.get('/checkshit', function(req, res){
-	res.send(storedshit);
 });
 
 app.listen(process.env.PORT || 8888, function () { console.log('WE GO NOW'); });
