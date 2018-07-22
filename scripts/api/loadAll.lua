a885dd9600e9e9784b99dcf6f64d5a85fc821274
Simon W <mister_simon@hotmail.co.uk>
2018-07-23 00:25:11 +0100

    Avoid loading everything from cwd, because that could involve loading EVERYTHING IN THE FILESYSTEM apparently.

--- scripts/api/loadAll.lua
+++ scripts/api/loadAll.lua
@@ -1,7 +1,7 @@
 -- Load all of the APIs within the APIs folder
 os.loadAPI("scripts/api/lib/helpers")
 
-local apiFiles = helpers.getFiles(shell.dir())
+local apiFiles = helpers.getFiles("scripts/api")
 
 for i=1,#apiFiles do
     os.loadAPI(apiFiles[i])
