03731f9e668ec131c390bc8fd664c624e0390e9b
Simon W <mister_simon@hotmail.co.uk>
2018-07-23 00:35:16 +0100

    Don't attempt to load the api loader script as an API because it's already loaded, ok? Yeah, then go to bed. For real this time.

--- scripts/api/loadAll.lua
+++ scripts/api/loadAll.lua
@@ -1,8 +1,11 @@
 -- Load all of the APIs within the APIs folder
 os.loadAPI("scripts/api/lib/helpers")
 
+local currentScript = shell.getRunningProgram()
 local apiFiles = helpers.getFiles("scripts/api")
 
 for i=1,#apiFiles do
-    pcall(function() os.loadAPI(apiFiles[i]) end)
+    if apiFiles[i] ~= currentScript then
+        os.loadAPI(apiFiles[i])
+    end
 end
