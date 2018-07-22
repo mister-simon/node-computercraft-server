c4e5c1edf061ee210ba31af513b15e30b2008b75
Simon W <mister_simon@hotmail.co.uk>
2018-07-23 00:22:45 +0100

    camelCaseIsInconsistentWhenItComesToAcronymsLikeAPIAndSuch

--- scripts/api/loadAll.lua
+++ scripts/api/loadAll.lua
@@ -1,8 +1,8 @@
 -- Load all of the APIs within the APIs folder
-os.loadApi("scripts/api/lib/helpers")
+os.loadAPI("scripts/api/lib/helpers")
 
 local apiFiles = helpers.getFiles(shell.dir())
 
 for i=1,#apiFiles do
-    os.loadApi(apiFiles[i])
+    os.loadAPI(apiFiles[i])
 end
