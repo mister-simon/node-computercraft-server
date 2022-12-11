09e5c6a767c88a820e3c47a07be8f85fd3f5d0bd
Simon W <mister_simon@hotmail.co.uk>
2022-12-11 02:48:59 +0000

    Handling nil date set when checking last check

--- startup/getGitIfUpdated.lua
+++ startup/getGitIfUpdated.lua
@@ -1,5 +1,9 @@
 local date = loadfile("startup/gitPulledAt.txt")
 
+if date == nil then
+    loadfile("scripts/getAllScriptsGit.lua")()
+end
+
 local commits = textutils.unserialiseJSON(
     http.get("https://api.github.com/repos/mister-simon/node-computercraft-server/commits?since="..date).fetchAll()
 )
