d618b0b290dd180cfd1f6622a11fc7eca159a417
Simon W <mister_simon@hotmail.co.uk>
2022-12-11 03:20:10 +0000

    Startup script to only refresh files if git commits don't match.

--- startup/getGitIfUpdated.lua
+++ startup/getGitIfUpdated.lua
@@ -1,11 +1,30 @@
-local date = loadfile("startup/gitPulledAt.txt")
+local file = fs.open("startup/sha.txt", "r")
+local commit = nil
+local latestCommit = nil
 
-if date == nil then
-    loadfile("scripts/getAllScriptsGit.lua")()
+if (file ~= nil) then
+    commit = file.readAll()
+    file.close()
+end
+
+if commit == nil then
+    dofile("scripts/getAllScriptsGit.lua")
 end
 
 local commits = textutils.unserialiseJSON(
-    http.get("https://api.github.com/repos/mister-simon/node-computercraft-server/commits?since="..date).fetchAll()
+    http.get("https://api.github.com/repos/mister-simon/node-computercraft-server/commits?per_page=1").readAll()
 )
 
-print(commits)
+if (commits[1] ~= nil) then
+    latestCommit = commits[1]["sha"]
+
+    local file_handle = fs.open("startup/sha.txt", "w");
+    file_handle.write(latestCommit)
+    file_handle.close()
+end
+
+if (commit ~= latestCommit) then
+    dofile("scripts/getAllScriptsGit.lua")
+else
+    print("Up to date.")
+end
