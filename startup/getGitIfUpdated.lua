134c6ef89ea76f071952680de3663f8f0c6238b9
Simon W <4751279+mister-simon@users.noreply.github.com>
2025-07-13 20:10:56 +0100

    Commenting out get git if updated startup script

--- startup/getGitIfUpdated.lua
+++ startup/getGitIfUpdated.lua
@@ -1,30 +1,32 @@
-local file = fs.open("startup-sha.txt", "r")
-local commit = nil
-local latestCommit = nil
+-- Commenting everything out because I'm not convinced this is working...
 
-if (file ~= nil) then
-    commit = file.readAll()
-    file.close()
-end
+-- local file = fs.open("startup-sha.txt", "r")
+-- local commit = nil
+-- local latestCommit = nil
 
-if commit == nil then
-    dofile("scripts/getAllScriptsGit.lua")
-end
+-- if (file ~= nil) then
+--     commit = file.readAll()
+--     file.close()
+-- end
 
-local commits = textutils.unserialiseJSON(
-    http.get("https://api.github.com/repos/mister-simon/node-computercraft-server/commits?per_page=1").readAll()
-)
+-- if commit == nil then
+--     dofile("scripts/getAllScriptsGit.lua")
+-- end
 
-if (commits[1] ~= nil) then
-    latestCommit = commits[1]["sha"]
+-- local commits = textutils.unserialiseJSON(
+--     http.get("https://api.github.com/repos/mister-simon/node-computercraft-server/commits?per_page=1").readAll()
+-- )
 
-    local file_handle = fs.open("startup-sha.txt", "w");
-    file_handle.write(latestCommit)
-    file_handle.close()
-end
+-- if (commits[1] ~= nil) then
+--     latestCommit = commits[1]["sha"]
 
-if (commit ~= latestCommit) then
-    dofile("scripts/getAllScriptsGit.lua")
-else
-    print("Up to date.")
-end
+--     local file_handle = fs.open("startup-sha.txt", "w");
+--     file_handle.write(latestCommit)
+--     file_handle.close()
+-- end
+
+-- if (commit ~= latestCommit) then
+--     dofile("scripts/getAllScriptsGit.lua")
+-- else
+--     print("Up to date.")
+-- end
