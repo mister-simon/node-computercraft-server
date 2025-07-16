a1f0efb5adcc4bcec3840281d0cc9f3dfb78cfea
Simon W <4751279+mister-simon@users.noreply.github.com>
2025-07-16 12:44:43 +0100

    Adds array helpers and a NAS module for interacting with a specific network of inventories

---
+++ scripts/api/arr.lua
@@ -0,0 +1,98 @@
+-- Insert into numeric or string index
+local function insert(out, index, value)
+    if type(index) == "number" then
+        table.insert(out, index, value)
+        return
+    end
+
+    if type(index) == "string" then
+        out[index] = value
+        return
+    end
+
+    error("Unusable index type:" .. type(index))
+end
+
+-- Run a callback across a table
+local function each(arr, callback)
+    for i, item in pairs(arr) do
+        callback(item, i)
+    end
+end
+
+-- Map across a table
+local function map(arr, callback)
+    local out = {}
+
+    each(arr, function(item, i)
+        insert(out, i, callback(item, i))
+    end)
+
+    return out
+end
+
+-- Create a new table from a table, stripping existing indexes
+local function values(arr)
+    local out = {}
+
+    each(arr, function(item)
+        table.insert(out, item)
+    end)
+
+    return out
+end
+
+-- Filter a table with a whitelisting callback
+local function filter(arr, callback)
+    local out = {}
+
+    for i, item in pairs(arr) do
+        if callback(item, i) then
+            insert(out, i, item)
+        end
+    end
+
+    return out
+end
+
+-- Filter a table with a blacklisting callback
+local function reject(arr, callback)
+    return filter(
+        arr,
+        function(item, i)
+            return not callback(item, i)
+        end
+    )
+end
+
+-- Truth test a callback against a table
+local function some(arr, callback)
+    for i, item in pairs(arr) do
+        if callback(item, i) then
+            return true
+        end
+    end
+    return false
+end
+
+-- Return the first table item matching a callback
+local function find(arr, callback)
+    for i, item in pairs(arr) do
+        if callback(item, i) then
+            return item, i
+        end
+    end
+    return nil
+end
+
+
+return {
+    insert = insert,
+    each = each,
+    map = map,
+    values = values,
+    filter = filter,
+    reject = reject,
+    some = some,
+    find = find,
+}
--- scripts/minedb/nas.lua
+++ scripts/minedb/nas.lua
@@ -1,1 +1,106 @@
+local arr = require("/scripts/api/arr")
+
 -- All things to do with interacting with networked storage can live here
+local function getNas(modemSide)
+    local remotes = peripheral.call(modemSide, "getNamesRemote")
+    local inputName, outputName
+
+    local getAll = function()
+        return {
+            peripheral.find(
+                "inventory",
+                function(name, inv)
+                    return arr.some(remotes, function(remote)
+                        return remote == name
+                    end)
+                end
+            )
+        }
+    end
+
+    local getInput = function()
+        assert(
+            inputName ~= nil,
+            "No input name set.\nCall setInputName with the unique inventory name.\nInventory must be on the same network!"
+        )
+
+        return arr.find(
+            getAll(),
+            function(inv)
+                return peripheral.getName(inv) == inputName
+            end
+        )
+    end
+
+    local getOutput = function()
+        assert(
+            outputName ~= nil,
+            "No output name set.\nCall setOutputName with the unique inventory name.\nInventory must be on the same network!"
+        )
+
+        return arr.find(
+            getAll(),
+            function(inv)
+                return peripheral.getName(inv) == outputName
+            end
+        )
+    end
+
+    local getStorage = function()
+        local input = getInput()
+        local output = getOutput()
+
+        return arr.reject(
+            getAll(),
+            function(item)
+                return item == input or item == output
+            end
+        )
+    end
+
+    -- Currently does will not differentiate NBT data
+    local list = function()
+        local items = {}
+
+        local function newItem()
+            local locations = {}
+            return {
+                getCount = function()
+                    local count = 0
+                    arr.each(locations, function(location)
+                        count = count + location.item.count
+                    end)
+                    return count
+                end,
+                addItem = function(inv, item, slot)
+                    table.insert(locations, { item = item, inv = inv, slot = slot })
+                end
+            }
+        end
+
+        arr.each(getStorage(), function(inv, i)
+            arr.each(inv.list(), function(item, slot)
+                if not items[item.name] then
+                    items[item.name] = newItem()
+                end
+
+                items[item.name].addItem(inv, item, slot)
+            end)
+        end)
+
+        return items
+    end
+
+    return {
+        getAll = getAll,
+        setInputName = function(name) inputName = name end,
+        getInput = getInput,
+        setOutputName = function(name) outputName = name end,
+        getOutput = getOutput,
+        getStorage = getStorage,
+        list = list,
+    }
+end
+
+
+return getNas
